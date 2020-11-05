dd31cefc9c9e5cf1ecca09660b2a7a7f09aa0a5b
Aaron Ratnavel <aaron.ratnavel@GCCYJQCBJ1WK.local>
2020-11-05 09:45:17 -0500

    template

---
+++ Project_1_Work/basketballinfo.py
@@ -0,0 +1,38 @@
+
+# https://stackoverflow.com/questions/20822553/align-tabs-from-right-to-left-using-ttk-notebook-widget
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.minsize(300, 300)
+root.geometry("1000x700")
+
+style = ttk.Style()
+style.configure('TNotebook', tabposition='nw') #'nw' as in compass direction
+# style.configure('lefttab.TNotebook', tabposition='ws')
+
+planner = ttk.Notebook(root, width=1000, height=650)
+
+# Create the frames of different colours that are 200*200 pixels in dimensions
+tab1 = tk.Frame(planner, bg='#00E8FF', width=200, height=200)
+tab2 = tk.Frame(planner, bg='#00E8FF', width=200, height=200)
+tab3 = tk.Frame(planner, bg='#00E8FF', width=200, height=200)
+tab4 = tk.Frame(planner, bg='#00E8FF', width=200, height=200)
+tab5 = tk.Frame(planner, bg='#00E8FF', width=200, height=200)
+
+
+
+# Add the tabs/frames to the notebook object called "planner" 
+planner.add(tab1, text='Home')
+planner.add(tab2, text='Stats')
+planner.add(tab3, text='Awards')
+planner.add(tab4, text="Teams")
+planner.add(tab5, text="Seasons")
+
+# Tabs will be added to the "top" of the "frame"
+planner.pack(side=tk.TOP)
+
+root.mainloop()
+
+
